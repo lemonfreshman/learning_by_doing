14f9323340004b306a9967776a326965ac2c75f8
Riley Jones <rileymjones@gmail.com>
2015-10-17 16:25:17 -0600

    Did some more formatting and made travis happy [#105353922]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -10,7 +10,7 @@ adjective1 = ask_madlib("an adjective")
 adjective2 = ask_madlib("another adjective")
 noun1 = ask_madlib("a noun")
 noun2 = ask_madlib("another noun")
-pluralnoun = ask_madlib("an plural noun")
+pluralnoun1 = ask_madlib("an plural noun")
 game = ask_madlib("a game")
 pluralnoun2 = ask_madlib("another plural noun")
 verb1 = ask_madlib("a verb ending in 'ing'")
@@ -27,24 +27,27 @@ number = ask_madlib("a number")
 pluralnoun4 = ask_madlib("another plural noun")
 
 
-
-puts "Here is your madlib:"
-puts "A vacation is when you take a trip to some " + adjective1 + " place
-with your " + adjective2 + " family. Usually you go to some place
-that is near a/an " + noun1 + " or up on a/an " + noun2 + ".
-A good vacation place is one where you can ride " + pluralnoun + "
-or play " + game + " or go hunting for " + pluralnoun2 + ". I like
-to spend my time " + verb1  + " or " + verb2 + ".
+puts <<END_OF_MADLIB
+Here's your madlib:
+A vacation is when you take a trip to some #{adjective1} place
+with your #{adjective2} family. Usually you go to some place
+that is near a/an #{noun1} or up on a/an #{noun2}.
+A good vacation place is one where you can ride #{pluralnoun1}
+or play #{game} or go hunting for #{pluralnoun2}. I like
+to spend my time #{verb1} or #{verb2}.
 When parents go on a vacation, they spend their time eating
-three " + pluralnoun3 +  " a day, and fathers play golf, and mothers
-sit around " + verb3  + ". Last summer, my little brother
-fell in a/an " +  noun3  + " and got poison " +  plant  + " all
-over his " + bodypart + ". My family is going to go to (the) "
-puts  place  + ", and I will practice " +  verb4  + ". Parents
+three #{pluralnoun3} a day, and fathers play golf, and mothers
+sit around #{verb3}. Last summer, my little brother
+fell in a/an #{noun3} and got poison #{plant} all
+over his #{bodypart}. My family is going to go to (the)
+#{place}, and I will practice #{verb4}. Parents
 need vacations more than kids because parents are always very
-" + adjective3 + " and because they have to work " + number + "
-hours every day all year making enough " + pluralnoun4 + "to pay
-for the vacation."
+#{adjective3} and because they have to work #{number}
+hours every day all year making enough #{pluralnoun4} to pay
+for the vacation.
+
+END_OF_MADLIB
+
 
 =begin
 A vacation is when you take a trip to some horrible place
