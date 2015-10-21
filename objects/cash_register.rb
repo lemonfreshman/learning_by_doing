a7b196ab4338f0688995586236f43b58b83a224f
Riley Jones <rileymjones@gmail.com>
2015-10-21 15:02:20 -0600

    Working cash register [#105361900]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -1,26 +1,26 @@
-class cashregister
+class CashRegister
+
+  attr_reader :total
+
   def initialize
-    contents = 0
+    @total = 0.00
   end
 
-  def purchase(amount.float)
-    @total += amount
+  def purchase(amount_owed)
+    @total = total + amount_owed
   end
 
-  def total
-    return @total
-  end
-
-  def pay(amount.float)
-    amount - @total
-    return change
+  def pay(amount_paid)
+    @change = amount_paid - @total
+    puts "Your change is $" + @change.to_s
   end
 
 end
 
-
+=begin
 Create a program called "cash_register.rb".
           Create a CashRegister class
                 purchase method takes a floating number and adds that to the total
                 total method returns how much is owed
                 pay method takes one floating number for how much is paid, should return how much change is given
+=end
